4f02d3773ba0496754dc6e9e071cd78796faec98
Charan Sritej <charansritej@gmail.com>
2020-05-14 18:47:31 +1000

    used sys args for getting data used at prompt

--- cnt_ec2_cde/shotty.py
+++ cnt_ec2_cde/shotty.py
@@ -1,8 +1,13 @@
 import boto3
+import sys
 
-if __name__=='__main__':
-    session = boto3.Session(profile_name = 'Chitti')
-    ec3 = session.resource('ec2')
+session = boto3.Session(profile_name = 'Chitti')
+ec3 = session.resource('ec2')
 
+def list_instances():
     for i in ec3.instances.all():
         print(i)
+
+if __name__=='__main__':
+    print(sys.argv)
+    list_instances()
