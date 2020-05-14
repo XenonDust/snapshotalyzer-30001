0c705856cb94834927d1cac2ddcb263bcb4f3459
Charan Sritej <charansritej@gmail.com>
2020-05-15 00:13:25 +1000

    setting tag into a dictionary and comitting, adding project filter in cli using click

--- cnt_ec2_cde/shotty.py
+++ cnt_ec2_cde/shotty.py
@@ -5,15 +5,25 @@ session = boto3.Session(profile_name = 'Chitti')
 ec3 = session.resource('ec2')
 
 @click.command()
-def list_instances():
+@click.option('--project', default=None,
+    help="only instances for project (tag Project:<name>)")
+def list_instances(project):
     "List EC2 instances"
+    instances=[]
+    if project:
+        filters = [{'Name':'tag:Project','Values':[project]}]
+        instances = ec3.instances.filter(Filters=filters)
+    else:
+        instances = ec3.instances
     for i in ec3.instances.all():
+        tags = {t['Key']:t['Value']} for t in i.tags or []}
         print(', '.join((
         i.id,
         i.instance_type,
         i.placement['AvailabilityZone'],
         i.state['Name'],
         i.public_dns_name
+        tags.get('Project','<No Project>')
         ))
         )
 
